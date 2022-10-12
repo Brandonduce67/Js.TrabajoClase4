3a320b88082300651986f2444f67bbc2106d1993
Brandonduce <Brandonduce12@gmail.com>
2022-10-12 11:10:42 -0300

    mi primer commit

---
+++ index.js
@@ -0,0 +1,141 @@
+let opcion = prompt ("Cines showcase, su lugar en el mundo. \n Seleccione una opcion: \n 1: Cartelera \n 2: Entradas \n 3: Contacto \n 4: Preguntas frecuentes \n 5: Mi cuenta \n 0: Salir")
+do {
+if (opcion == 1) {
+
+    alert ("Peliculas: \n Gaturro \n Superman \n Batman")
+    break
+
+} else if (opcion == 2) {
+
+    let entradas = prompt ("Venta de entradas\nSeleccione su pelicula:\n1: Gaturro\n2: Superman\n3: Batman")
+    if (entradas == 1) {
+
+        let gaturro = prompt ("Pelicula: Gaturro:\nSeleccione en que formato quiere ver su pelicula:\n1: 2D (400$)\n2: 3D (500$)\n3: 4D (600$)")
+        if (gaturro == 1) {
+            let dosde = prompt ("Pelicula: Gaturro. Formato: 2D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (dosde == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 2D. Establecimiento: Moron\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (dosde == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 2D. Establecimiento: Moreno\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (dosde == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 2D. Establecimiento: Padua\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        } else if (gaturro == 2) {
+            let tresde = prompt ("Pelicula: Gaturro. Formato: 3D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (tresde == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 3D. Establecimiento: Moron\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (tresde == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 3D. Establecimiento: Moreno\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (tresde == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 3D. Establecimiento: Padua\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        } else if (gaturro == 3) {
+            let cuatrode = prompt ("Pelicula: Gaturro. Formato: 4D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (cuatrode == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 4D. Establecimiento: Moron\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (cuatrode == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 4D. Establecimiento: Moreno\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (cuatrode == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Gaturro. Formato: 4D. Establecimiento: Padua\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        }
+
+    } else if (entradas == 2) {
+
+        let Superman = prompt ("Pelicula: Superman:\nSeleccione en que formato quiere ver su pelicula:\n1: 2D (400$)\n2: 3D (500$)\n3: 4D (600$)")
+        if (Superman == 1) {
+            let dosde = prompt ("Pelicula: Superman. Formato: 2D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (dosde == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 2D. Establecimiento: Moron\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (dosde == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 2D. Establecimiento: Moreno\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (dosde == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 2D. Establecimiento: Padua\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        } else if (Superman == 2) {
+            let tresde = prompt ("Pelicula: Superman. Formato: 3D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (tresde == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 3D. Establecimiento: Moron\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (tresde == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 3D. Establecimiento: Moreno\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (tresde == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 3D. Establecimiento: Padua\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        } else if (Superman == 3) {
+            let cuatrode = prompt ("Pelicula: Superman. Formato: 4D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (cuatrode == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 4D. Establecimiento: Moron\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (cuatrode == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 4D. Establecimiento: Moreno\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (cuatrode == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Superman. Formato: 4D. Establecimiento: Padua\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        }
+
+
+    } else if (entradas == 3) {
+
+        let Batman = prompt ("Pelicula: Batman:\nSeleccione en que formato quiere ver su pelicula:\n1: 2D (400$)\n2: 3D (500$)\n3: 4D (600$)")
+        if (Batman == 1) {
+            let dosde = prompt ("Pelicula: Batman. Formato: 2D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (dosde == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 2D. Establecimiento: Moron\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (dosde == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 2D. Establecimiento: Moreno\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (dosde == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 2D. Establecimiento: Padua\nSon 400$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        } else if (Batman == 2) {
+            let tresde = prompt ("Pelicula: Batman. Formato: 3D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (tresde == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 3D. Establecimiento: Moron\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (tresde == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 3D. Establecimiento: Moreno\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (tresde == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 3D. Establecimiento: Padua\nSon 500$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        } else if (Batman == 3) {
+            let cuatrode = prompt ("Pelicula: Batman. Formato: 4D.\nSeleccione establecimiento:\n1: Moron\n2: Moreno\n3: Padua")
+            if (cuatrode == 1) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 4D. Establecimiento: Moron\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (cuatrode == 2) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 4D. Establecimiento: Moreno\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            } else if (cuatrode == 3) {
+                alert ("Felicidades usted termino el proceso de la compra de su entrada..\nPelicula: Batman. Formato: 4D. Establecimiento: Padua\nSon 600$$ \n!!Que disfrute de su pelicula¡¡")
+            }
+        }
+
+    }
+    break
+
+} else if (opcion == 3) {
+
+    alert("Esperamos tu consulta\n CINES SHOWCASE\n Email: info@gmail.com\n Instagram: @cineshowcase")
+    break
+
+} else if (opcion == 4) {
+
+    alert("PREGUNTAS FRECUENTES\n + ¿Tengo reemvolso de mis entradas?\n + ¿Puedo cambiar el dia de día, cine, horario, localidades o película de las entradas que compré?\n + Me olvidé de comprar comidas ¿Puedo agregarlas a mi compra?")
+    break
+
+} else if (opcion == 5) {
+
+    let correo = prompt ("Inicie secion con su cuenta y contraseña.\n Correo electronico:")
+    let contrasenia = prompt ("Contraseña:")
+    alert ("Bienvenido disfrute de su estancia aqui!!!\nSu correo electronico es :" + correo + "\nSu contraseña es: " + contrasenia)
+    break
+
+}
+} while (opcion != 0);
+
+
+
+
+
+
+
+
+
+
+
+
